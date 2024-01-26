9815a704f32bdd8db1a38953afab26ef6379ecf7
Joe Rafaniello <jrafanie@gmail.com>
2024-01-26 17:07:17 -0500

    require_nested is no longer needed with zeitwerk

--- app/models/manageiq/providers/nsxt/inventory/parser.rb
+++ app/models/manageiq/providers/nsxt/inventory/parser.rb
@@ -1,3 +1,2 @@
 class ManageIQ::Providers::Nsxt::Inventory::Parser < ManageIQ::Providers::Inventory::Parser
-  require_nested :NetworkManager
 end
--- app/models/manageiq/providers/nsxt/inventory/persister.rb
+++ app/models/manageiq/providers/nsxt/inventory/persister.rb
@@ -1,4 +1,2 @@
 class ManageIQ::Providers::Nsxt::Inventory::Persister < ManageIQ::Providers::Inventory::Persister
-  require_nested :NetworkManager
-  require_nested :TargetCollection
 end
