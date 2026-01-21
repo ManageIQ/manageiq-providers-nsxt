5d1df2762f284da747181aa5a1bc85cd9ab760f2
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-01-21 12:20:25 +0530

    Carbon 11 migration: delete-nsxt-security-group-form

--- app/javascript/components/security-groups/delete-nsxt-security-group-form.jsx
+++ app/javascript/components/security-groups/delete-nsxt-security-group-form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { ModalBody } from 'carbon-components-react';
+import { ModalBody } from '@carbon/react';
 import { SecurityGroupApi } from '../../utils/security-group-api';
 
 class DeleteNsxtSecurityGroupForm extends React.Component {
