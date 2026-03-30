a28189715201a017dee36b2c415cdbc2899f7f21
Adam Grare <adam@grare.com>
2026-03-30 09:02:17 -0400

    Delete unused `NetworkManager#create_*` methods
    These called into CRUD methods that were removed prior to the plugin
    being adopted due to not having the underlying ansible playbooks that
    execute the operations.

--- app/models/manageiq/providers/nsxt/network_manager.rb
+++ app/models/manageiq/providers/nsxt/network_manager.rb
@@ -25,20 +25,4 @@ class ManageIQ::Providers::Nsxt::NetworkManager < ManageIQ::Providers::NetworkMa
   def cloud_tenants
     ::CloudTenant.where(:ems_id => id)
   end
-
-  def create_cloud_network(cloud_network)
-    ManageIQ::Providers::Nsxt::NetworkManager::CloudNetwork.raw_create_cloud_network(self, cloud_network)
-  end
-
-  def create_security_group(security_group)
-    ManageIQ::Providers::Nsxt::NetworkManager::SecurityGroup.raw_create_security_group(self, security_group)
-  end
-
-  def create_security_policy(security_policy)
-    ManageIQ::Providers::Nsxt::NetworkManager::SecurityPolicy.raw_create_security_policy(self, security_policy)
-  end
-
-  def create_security_policy_rule(security_policy_rule)
-    ManageIQ::Providers::Nsxt::NetworkManager::SecurityPolicyRule.raw_create_security_policy_rule(self, security_policy_rule)
-  end
 end
