d49b438009a97155478a19942eb1367638f425b8
asirvadAbrahamVarghese <Asirvad.Abraham.Varghese@ibm.com>
2026-05-26 16:22:19 +0530

    Drop Ruby button helpers

--- app/helpers/helper/application_helper/button/any_nsxt_network_manager.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationHelper::Button::AnyNsxtNetworkManager < ApplicationHelper::Button::Basic
-  def visible?
-    Rbac.filtered(ManageIQ::Providers::Nsxt::NetworkManager).any?
-  end
-end
--- app/helpers/helper/application_helper/button/belongs_to_nsxt_network_manager.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationHelper::Button::BelongsToNsxtNetworkManager < ApplicationHelper::Button::Basic
-  needs(:@record)
-
-  def visible?
-    @record.class.module_parent == ManageIQ::Providers::Nsxt::NetworkManager
-  end
-end
